c8bcce424641ce9e6207345f0ce0326ef7fb7920
“Yykai8899” <18123859523@163.com>
2025-06-05 05:14:09 +0800

    作业提交

--- task-go/go_base_2/main.go
+++ task-go/go_base_2/main.go
@@ -3,6 +3,8 @@ package main
 import (
 	"fmt"
 	"sync"
+	"sync/atomic"
+	"time"
 )
 
 func main() {
@@ -18,6 +20,24 @@ func main() {
 
 	// Goroutine作业1
 	goroutine1()
+
+	// Goroutine作业2
+	taskScheduler()
+
+	// 面向对象作业1、2
+	obj()
+
+	// channel作业1
+	channel1()
+
+	// channel作业2
+	channel2()
+
+	// 锁机制作业1
+	lock1()
+
+	// 锁机制作业2
+	lock2()
 }
 
 // 编写一个Go程序，定义一个函数，该函数接收一个整数指针作为参数，在函数内部将该指针指向的值增加10，然后在主函数中调用该函数并输出修改后的值。
@@ -64,28 +84,8 @@ func goroutine1() {
 	}()
 	wg.Wait()
 
-	// 面向对象 1
-	// 先打印结构体的值
-	var rectangle Rectangle
-	fmt.Println("Rectangle.a:", rectangle.a)
-	fmt.Println("Rectangle.b:", rectangle.b)
-	var circle Circle
-	fmt.Println("Circle.c:", circle.c)
-	fmt.Println("Circle.d:", circle.d)
-	rectangle = Rectangle{a: 1, b: "2"}
+	// Channel
 
-	// 执行方法
-	rectangle.Area()
-	circle.Perimeter()
-	// 验证接口是否执行，值修改了就是执行了
-	fmt.Println("new Rectangle.a:", rectangle.a)
-	fmt.Println("new Rectangle.b:", rectangle.b)
-	fmt.Println("new Circle.c:", circle.c)
-	fmt.Println("new Circle.d:", circle.d)
-
-	// 面向对象2
-	emplInfo := Employee{Person{"JXD", 18}, 2131231}
-	emplInfo.PrintInfo()
 }
 
 type Shape interface {
@@ -142,4 +142,171 @@ func (e Employee) PrintInfo() {
 	fmt.Println("员工姓名:", e.psn.Name)
 	fmt.Println("员工年龄:", e.psn.Age)
 	fmt.Println("员工ID:", e.employeeID)
+}
+
+// 任务调度器
+func taskScheduler() {
+	type Task struct {
+		Name string
+		Func func()
+	}
+
+	var wg sync.WaitGroup
+
+	method := func(tasks []Task) {
+		wg.Add(len(tasks))
+
+		for _, task := range tasks {
+			go func(t Task) {
+				defer wg.Done() // 确保任务完成时调用 Done
+
+				start := time.Now()
+				fmt.Printf("开始执行任务: %s\n", t.Name)
+				t.Func()
+				duration := time.Since(start)
+				fmt.Printf("任务 %s 执行时间: %v\n", t.Name, duration)
+			}(task)
+		}
+	}
+	tasks := []Task{
+		{
+			Name: "任务1",
+			Func: func() {
+				time.Sleep(time.Second * 2)
+			},
+		},
+		{
+			Name: "任务2",
+			Func: func() {
+				time.Sleep(time.Second * 3)
+			},
+		},
+	}
+	method(tasks) // 启动任务调度器
+	wg.Wait()
+}
+
+func obj() {
+	// 面向对象 1
+	// 先打印结构体的值
+	var rectangle Rectangle
+	fmt.Println("Rectangle.a:", rectangle.a)
+	fmt.Println("Rectangle.b:", rectangle.b)
+	var circle Circle
+	fmt.Println("Circle.c:", circle.c)
+	fmt.Println("Circle.d:", circle.d)
+	rectangle = Rectangle{a: 1, b: "2"}
+
+	// 执行方法
+	rectangle.Area()
+	circle.Perimeter()
+	// 验证接口是否执行，值修改了就是执行了
+	fmt.Println("new Rectangle.a:", rectangle.a)
+	fmt.Println("new Rectangle.b:", rectangle.b)
+	fmt.Println("new Circle.c:", circle.c)
+	fmt.Println("new Circle.d:", circle.d)
+
+	// 面向对象2
+	emplInfo := Employee{Person{"JXD", 18}, 2131231}
+	emplInfo.PrintInfo()
+}
+
+func channel1() {
+	method := func() {
+		var ch = make(chan int)
+		go func() {
+			for i := 1; i <= 10; i++ {
+				ch <- i // 将整数发送到通道
+			}
+			fmt.Println("发送完毕，关闭通道")
+			close(ch) // 关闭通道
+		}()
+
+		go func() {
+			for {
+				select {
+				case num, ok := <-ch:
+					if ok {
+						fmt.Println("接收到的整数:", num) // 从通道接收整数并打印
+					} else {
+						fmt.Println("通道已关闭")
+						return
+					}
+				default:
+					fmt.Println("等待接收数据...")
+					time.Sleep(100 * time.Millisecond) // 避免忙等待
+				}
+			}
+		}()
+	}
+
+	method()
+	time.Sleep(2 * time.Second) // 确保协程有时间执行
+}
+
+func channel2() {
+	method := func() {
+		ch := make(chan int, 10) // 创建一个缓冲通道，容量为10
+
+		go func() {
+			for i := 1; i <= 100; i++ {
+				ch <- i // 生产者将整数发送到通道
+				fmt.Println("生产者发送:", i)
+			}
+			close(ch) // 关闭通道
+		}()
+
+		go func() {
+			for num := range ch { // 从通道接收整数
+				fmt.Println("消费者接收到:", num)
+			}
+			fmt.Println("消费者完成接收")
+		}()
+	}
+
+	method()
+	time.Sleep(5 * time.Second) // 确保协程有时间执行
+}
+
+type Counter struct {
+	mu    sync.Mutex // 互斥锁
+	count int
+}
+
+func (c *Counter) Increment() {
+	c.mu.Lock()         // 上锁
+	defer c.mu.Unlock() // 确保在函数结束时解锁
+	c.count++           // 递增计数器
+}
+
+// 锁机制1
+func lock1() {
+	counter := &Counter{}
+
+	for i := 0; i < 10; i++ {
+		go func() {
+			for j := 0; j < 1000; j++ {
+				counter.Increment() // 每个协程递增计数器
+			}
+		}()
+	}
+
+	time.Sleep(2 * time.Second)            // 等待所有协程完成
+	fmt.Println("计数器的最终值:", counter.count) // 输出计数器的值
+}
+
+// 锁机制2
+func lock2() {
+	var counter uint64
+
+	for i := 0; i < 10; i++ {
+		go func() {
+			for j := 0; j < 1000; j++ {
+				// 使用原子操作递增计数器
+				atomic.AddUint64(&counter, 1)
+			}
+		}()
+	}
+	time.Sleep(2 * time.Second)        // 等待所有协程完成
+	fmt.Println("无锁计数器的最终值:", counter) // 输出计数器的值
 }
